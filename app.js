3585b2f433b6b17f8cb41624d3eab822821b954e
RoyBuchanandev <93665139+RoyBuchanandev@users.noreply.github.com>
2023-01-04 23:01:10 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
 const express = require('express');
-const ProductManager = require('./ProductManager');
+const {ProductManager} = require('./manejoDeArchivos');
+
+const app = express();
+const port = 3002;
+
 
 const productManager = new ProductManager('products.json');
-const app = express();
+
+
+productManager.addProduct('Pelicula 1', 'Scarface', 1000, 'scarface.jpg', 'code1', 10);
+productManager.addProduct('Pelicula 2', '2001 Odisea en el espacio', 1200, '2001 Odisea en el espacio.jpg', 'code2', 5);
+productManager.addProduct('Pelicula 3', 'Jurassic Park', 1500, '2001 Odisea en el espacio.jpg', 'code3', 0);
+
+
+productManager.writeToFile().then(() => {
+  console.log('Productos escritos al archivo');
+}).catch(error => {
+  console.error(error);
+});
 
 app.get('/products', (req, res) => {
   const limit = req.query.limit;
-  let products = productManager.getProducts();
-  if (limit) {
-    products = products.slice(0, limit);
-  }
-  res.send({ products });
+  productManager.readFromFile().then(() => {
+    let products = productManager.getProducts();
+    if (limit) {
+      products = products.slice(0, limit);
+    }
+    res.send(products);
+  }).catch(error => {
+    console.error(error);
+  });
 });
 
 app.get('/products/:pid', (req, res) => {
   const pid = req.params.pid;
-  const product = productManager.getProductById(pid);
-  if (!product) {
-    console.log('no se encontro el producto');
-  } else {
-    res.send({ product });
-  }
+  productManager
+    .readFromFile()
+    .then(() => {
+      const product = productManager.getProductById(pid);
+      if (product) {
+        res.send(product);
+      } else {
+       console.log('no se encontro el producto');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      console.log('no se pudieron leer los productos')
+    });
 });
 
-app.listen(3000, () => {
-    console.log('Servidor iniciado en el puerto 3000');
-  });
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
--- manejoDeArchivos.js
+++ manejoDeArchivos.js
@@ -1,90 +1,74 @@
-const fs = require('node:fs');
+const fs = require('fs');
 
 class ProductManager {
-    static counter = 0;
-    products = [];
-    path;
+  static counter = 0;
+  products = [];
+  path;
 
-    constructor(path) {
-        this.path = path;
+  constructor(path) {
+    this.path = path;
+  }
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+    const id = ProductManager.counter += 1;
+    const product = { id, title, description, price, thumbnail, code, stock };
+    this.products.push(product);
+    return product;
+  }
+
+  getProducts() {
+    return [...this.products];
+  }
+
+  getProductById(id) {
+    return this.products.find(product => product.id === id);
+  }
+
+  updateProduct(id, updates) {
+    const product = this.getProductById(id);
+    if (!product) {
+      throw new Error(`Product with id ${id} not found`);
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-        const id = ProductManager.counter += 1;
-        const product = { id, title, description, price, thumbnail, code, stock };
-        this.products.push(product);
-        return product;
+    for (const [key, value] of Object.entries(updates)) {
+      product[key] = value;
     }
 
-    getProducts() {
-        return [...this.products];
-    }
+    return product;
+  }
 
-    getProductById(id) {
-        return this.products.find(product => product.id === id);
+  deleteProduct(id) {
+    const productIndex = this.products.findIndex(product => product.id === id);
+    if (productIndex === -1) {
+      throw new Error(`Product with id ${id} not found`);
     }
+    this.products.splice(productIndex, 1);
+  }
 
-    updateProduct(id, updates) {
-        const product = this.getProductById(id);
-        if (!product) {
-            throw new Error(`Product with id ${id} not found`);
+  writeToFile() {
+    return new Promise((resolve, reject) => {
+      fs.writeFile(this.path, JSON.stringify(this.products), error => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
         }
+      });
+    });
+  }
 
-        for (const [key, value] of Object.entries(updates)) {
-            product[key] = value;
+  readFromFile() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(this.path, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          this.products = JSON.parse(data);
+          resolve();
         }
-
-        return product;
-    }
-
-
-    deleteProduct(id) {
-        const productIndex = this.products.findIndex(product => product.id === id);
-        if (productIndex === -1) {
-            throw new Error(`Product with id ${id} not found`);
-        }
-        this.products.splice(productIndex, 1);
-    }
-
-    writeToFile() {
-        try {
-            fs.writeFileSync(this.path, JSON.stringify(this.products));
-        } catch (error) {
-            throw new Error(error)
-        }
-    }
-
-    readFromFile() {
-        try {
-            this.products = JSON.parse(fs.readFileSync(this.path));
-        } catch (error) {
-            throw new Error(error)
-        }
-
-    }
+      });
+    });
+  }
 }
 
-
-const productManager = new ProductManager('products.json');
-
-const product1 = productManager.addProduct('Entrada Metallica', 'Entrada para el recital de Metallica', 10, 'imagenRecital1.jpg', 'code1', 10);
-const product2 = productManager.addProduct('Entrada Megadeth', 'Entrada para el recital de Megadeth', 20, 'imagenRecital2.jpg', 'code2', 5);
-const product3 = productManager.addProduct('Entrada Slayer', 'Entrada para el recital de Slayer', 30, 'imagenRecital3.jpg', 'code3', 2);
-
-console.log(productManager.getProducts());
-
-console.log(productManager.getProductById(1));
-
-productManager.updateProduct(1, { title: 'Nuevo titulo' });
-console.log(productManager.getProductById(1));
-
-
-productManager.deleteProduct(1);
-console.log(productManager.getProductById(1));
-
-productManager.writeToFile()
-productManager.readFromFile()
-
-module.exports = {
-    ProductManager // exporto la clase instanciada y la exporto para tener el servidor en otro modulo
-}
+module.exports = {ProductManager};
